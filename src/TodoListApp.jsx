272c41be339ef8749f342f4fe2f76c1ff803b31f
omgini <0lzin0v0@gmail.com>
2026-04-06 14:35:46 +0900

    TodoListApp HTML

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,5 +1,28 @@
 function TodoListApp(){
+    return(
+        <div>
+            <h1>ToDo List</h1>
+            <form action="">
+                <input type="text" placeholder="할 일을 입력하세요."></input>
+                <button>Add</button>
+            </form>
 
-
+            <ul>
+                <li>
+                    <input type="checkbox" name="" id="chk-1"/>
+                    <label htmlFor="chk-1">놀기</label>
+                    <button>✏️</button>
+                    <button>❌</button>
+                    </li>
+                <li>
+                    <input type="checkbox" name="" id=""/>
+                    <label htmlFor="">집 가기</label>
+                    <button>✏️</button>
+                    <button>❌</button>
+                    </li>
+            </ul>
+        </div>
+    )
 }
-export default TodoListApp
+
+export default TodoListApp;
